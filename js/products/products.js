fddcae2d00a3edb2abe12cd2c869011baa4d83ce
Huber Guiza <andresufps1160809gmail.com>
2024-11-07 15:20:03 -0500

    add new product

--- js/products/products.js
+++ js/products/products.js
@@ -1,7 +1,8 @@
 const products = [
-    { name: 'Product 1', qty: 10 }
-    { name: 'Product 2', qty: 6 }
-    { name: 'Product 3', qty: 8 }
-    { name: 'Product 4', qty: 24}
+    { name: 'Product 1', qty: 10 },
+    { name: 'Product 2', qty: 6 },
+    { name: 'Product 3', qty: 8 },
+    { name: 'Product 4', qty: 24 },
+    { name: 'Product 5', qty: 36}
 ]
 
