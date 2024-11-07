d398704cd8c329b05fe25ebd81273b256e9f5348
Huber Guiza <andresufps1160809gmail.com>
2024-11-07 16:07:08 -0500

    add minQuantity in products.js

--- js/products/products.js
+++ js/products/products.js
@@ -3,7 +3,7 @@ const products = [
     { name: 'Product 2', qty: 6 },
     { name: 'Product 3', qty: 8 },
     { name: 'Product 4', qty: 24 },
-    { name: 'Product 5', qty: 36}
-]
+    { name: 'Product 5', qty: 36 }
+];
 
 const minQuantity = 10;
